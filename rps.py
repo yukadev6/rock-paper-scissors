c5ff8e45fe4464095c0df471ef780458369621ac
yukadev6 <76265252+yukadev6@users.noreply.github.com>
2024-02-27 20:26:10 -0600

    Added parent function

--- rps.py
+++ rps.py
@@ -3,63 +3,81 @@ import sys
 import random
 from enum import Enum
 
-game_count = 0
+def rps():
+    game_count = 0
+    player_wins = 0
+    python_wins = 0
 
-def play_rps():
-    class RPS(Enum):
-        ROCK = 1
-        PAPER = 2
-        SCISSORS = 3
+    def play_rps():
+        nonlocal player_wins
+        nonlocal python_wins
 
-    playerchoice = input(
-        "\nEnter... \n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n")
+        class RPS(Enum):
+            ROCK = 1
+            PAPER = 2
+            SCISSORS = 3
 
-    if playerchoice not in ["1","2","3"]:
-        print("You must enter 1, 2, or 3.")
-        return play_rps()
-    
-    player = int(playerchoice)
+        playerchoice = input(
+            "\nEnter... \n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n")
 
-    computerchoice = random.choice("123")
-    computer = int(computerchoice)
-
-    print("\nYou chose " + str(RPS(player)).replace('RPS.','') + ".")
-    print("Python chose " + str(RPS(computer)).replace('RPS.','') + ".")
-
-    def decide_winner(player, computer):
-        if player == 1 and computer == 3:
-            return "You win!"
-        elif player == 2 and computer == 1:
-            return "You win!"
-        elif player == 3 and computer == 2:
-            return "You win!"
-        elif player == computer:
-            return "Tie game!"
-        else:
-            print("Python wins!")
-
-    game_result = decide_winner(player, computer)
-
-    print(game_result)
-
-    global game_count
-    game_count += 1
-
-    print("\nGame count: " + str(game_count))
-          
-    print("\nPlay again?")
-
-    while True:
-        playagain = input("\nY for Yes or \nQ to Quit\n")
-        if playagain.lower() not in ["y", "q"]:
-            continue
-        else:
-            break
-
-    if playagain.lower() == "y":
+        if playerchoice not in ["1","2","3"]:
+            print("You must enter 1, 2, or 3.")
             return play_rps()
-    else:
-        print("Thank you for playing!\n")
-        sys.exit("Bye")
+        
+        player = int(playerchoice)
 
-play_rps()
+        computerchoice = random.choice("123")
+        computer = int(computerchoice)
+
+        print("\nYou chose " + str(RPS(player)).replace('RPS.','') + ".")
+        print("Python chose " + str(RPS(computer)).replace('RPS.','') + ".")
+
+        def decide_winner(player, computer):
+            nonlocal player_wins
+            nonlocal python_wins
+            if player == 1 and computer == 3:
+                player_wins += 1
+                return "You win!"
+            elif player == 2 and computer == 1:
+                player_wins += 1
+                return "You win!"
+            elif player == 3 and computer == 2:
+                player_wins += 1
+                return "You win!"
+            elif player == computer:
+                return "Tie game!"
+            else:
+                python_wins += 1
+                print("Python wins!")
+
+        game_result = decide_winner(player, computer)
+
+        print(game_result)
+
+        nonlocal game_count
+        game_count += 1
+
+        print("\nGame count: " + str(game_count))
+        print("\nPlayer wins: " + str(player_wins))
+        print("\nPython wins: " + str(python_wins))
+            
+        print("\nPlay again?")
+
+        while True:
+            playagain = input("\nY for Yes or \nQ to Quit\n")
+            if playagain.lower() not in ["y", "q"]:
+                continue
+            else:
+                break
+
+        if playagain.lower() == "y":
+                return play_rps()
+        else:
+            print("Thank you for playing!\n")
+            sys.exit("Bye")
+
+    return play_rps
+
+play = rps()
+
+play()
